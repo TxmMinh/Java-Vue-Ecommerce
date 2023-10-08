e42c78c3b20ffb81c055bc3e347ef800b533ec91
TxmMinh <xuanminh2107@gmail.com>
2023-10-08 10:07:43 +0700

    create: category-create-list

--- ecommerce-backend/src/main/java/com/ecommerce/backend/EcommerceBackendApplication.java
+++ ecommerce-backend/src/main/java/com/ecommerce/backend/EcommerceBackendApplication.java
@@ -1,4 +1,4 @@
-package com.ecommercebackend;
+package com.ecommerce.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ ecommerce-backend/src/main/java/com/ecommerce/backend/controller/CategoryController.java
@@ -0,0 +1,30 @@
+package com.ecommerce.backend.controller;
+
+import com.ecommerce.backend.model.Category;
+import com.ecommerce.backend.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @PostMapping("/create")
+    public String createCategory(@RequestBody Category category) {
+        categoryService.createCategory(category);
+        return "success";
+    }
+
+    @GetMapping("/list")
+    public List<Category> listCategory() {
+        return categoryService.listCategory();
+    }
+}
