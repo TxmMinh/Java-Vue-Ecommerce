13bd2c43edd6691d3ce0c912231906c7b322b50e
minhtx <minhtx@runsystem.net>
2024-03-02 11:09:20 +0700

    feat: update file DB

---
+++ DB/categories.sql
@@ -0,0 +1,14 @@
+/*
+-- Query: SELECT * FROM j_vue_ecommerce.categories
+LIMIT 0, 1000
+
+-- Date: 2024-03-02 11:08
+*/
+INSERT INTO `` (`id`,`category_name`,`description`,`image_url`) VALUES (1,'Bookstore','A bookstore is a store that sells books, and where people can buy them','https://www.forewordreviews.com/books/covers/my-bookstore.jpg');
+INSERT INTO `` (`id`,`category_name`,`description`,`image_url`) VALUES (2,'Phones',' a portable telephone that can make and receive calls over a radio frequency link while the user is moving within a telephone service area','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGV-3Xj5H1ILLfTlFovcvut4eCZVhoFopEOa6p4zkggkM_rANicamiCanKNClxf2s1ftk&usqp=CAU');
+INSERT INTO `` (`id`,`category_name`,`description`,`image_url`) VALUES (3,'Digital Devices','A digital device is a piece of physical equipment that uses digital data, such as by sending, receiving, storing or processing it','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8W7xlEJS6JcZJ3C6Bpa5JXtCfqqKANegbtw&usqp=CAU');
+INSERT INTO `` (`id`,`category_name`,`description`,`image_url`) VALUES (4,'Computer','A computer is an electronic device that manipulates information, or data.','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs3Whe2c1aWgaeA9SRqyBNojC5VhlTbr3bO1_8rDBQj79yYBH5BCP564bl2AzfEVvKhMw&usqp=CAU');
+INSERT INTO `` (`id`,`category_name`,`description`,`image_url`) VALUES (5,'Cars - Motorcycles - Bicycles','A vehicle is a machine such as a car, bus, or truck which has an engine and is used to carry people from place to place.','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXj31AyXH787UP2vRrKMVjZG_UM9TnQ23CiroPmYzL0GBHIUzOR0oRxC_klqFjTIMrkNY&usqp=CAU');
+INSERT INTO `` (`id`,`category_name`,`description`,`image_url`) VALUES (6,'Fashion','Fashion is a way of thinking and also a way of making','https://i.pinimg.com/736x/12/c0/a1/12c0a1a6a0542be5f69b1e48f530c50c.jpg');
+INSERT INTO `` (`id`,`category_name`,`description`,`image_url`) VALUES (7,'Watches','Discover watches you\'ve never seen before.','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKI2_J9ICtav5aqkubvuhcRDKX6SwJzyrG25ffCheGOd2y_2rNIiX5jhfVk2i_pZdwDBM&usqp=CAU');
+INSERT INTO `` (`id`,`category_name`,`description`,`image_url`) VALUES (8,'Sports','Sport pertains to any form of physical activity or game, often competitive and organized, that aims to use, maintain, or improve physical ability and skills','https://cdn.firstcry.com/education/2022/11/25154201/Learn-Sport-Names-List-of-Sports-in-English.jpg');
